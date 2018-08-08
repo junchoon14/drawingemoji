7e13bd05eecb5730f5e91c090647518b8aea35b1
Jason Hsu <junchoon14@gmail.com>
2018-08-08 22:18:00 +0800

    Initial Commit

---
+++ drawingemoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  drawingemoji
+//
+//  Created by Jason Hsu on 2018/8/8.
+//  Copyright © 2018 junchoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
